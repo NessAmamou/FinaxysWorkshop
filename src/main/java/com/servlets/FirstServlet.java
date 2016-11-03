8ce7e82cb4aa5ddb2ef0911ddc638ad38a16eae5
feliasfinaxys <finaxy@finaxys.com>
2016-11-03 15:06:35 +0100

    Initial Commit

---
+++ src/main/java/com/servlets/FirstServlet.java
@@ -0,0 +1,34 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+
+	 public  void doGet(HttpServletRequest request, HttpServletResponse response)
+	 throws ServletException, IOException  {
+	
+		response.setContentType("text/html") ;
+		
+		PrintWriter out = response.getWriter() ;
+		out.println("<html>") ;
+		out.println("<head>") ;
+		out.println("<title>Bonjour le monde !</title>") ;
+		out.println("</head>") ;
+		out.println("<body>") ;
+		out.println("<h1>Bonjour le monde !</h1>") ;
+		out.println("</body>") ;
+		out.println("</html>") ;   
+	}
+
+	 public  void doPost(HttpServletRequest request, HttpServletResponse response)
+	 throws ServletException, IOException  {
+	
+		doGet(request, response) ;
+	}
+}	
