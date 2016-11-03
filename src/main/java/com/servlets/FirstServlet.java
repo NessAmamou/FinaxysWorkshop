9a3de0dd652aea800f31ff5307a88dd3555d4748
NessAmamou <namamou@finaxys.com>
2016-11-03 17:55:51 +0100

    Revert Test3

--- src/main/java/com/servlets/FirstServlet.java
+++ src/main/java/com/servlets/FirstServlet.java
@@ -17,7 +17,7 @@ public class FirstServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter() ;
 		out.println("<html>") ;
-		out.println("<body>") 
+		out.println("<body>") ;
 		out.println("<h1>Test</h1>") ;
 		out.println("</body>") ;
 		out.println("</html>") ;   
